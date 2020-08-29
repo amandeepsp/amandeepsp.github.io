9d560b33dc767bf133dc2c6a79834c5ede003d65
Amandeep Singh <amandeepspdhr@gmail.com>
2020-08-29 14:20:50 +0530

    Moving to Gatsby

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default function (){
+    return(
+        <Layout>
+            <h1>404 not found</h1>
+        </Layout>
+    )
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const MOBILE_QUERY_SIZE = 768
