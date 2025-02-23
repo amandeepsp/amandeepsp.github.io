33ae7bc328714f0ae09fa43a0b5872e372afa1da
amandeepsp <amandeepspdhr@gmail.com>
2025-02-23 20:38:19 +0530

    feat: add Hero component for introduction

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -9,12 +9,19 @@ export type Link = {
     href: string;
 };
 
+export type Hero = {
+    title: string;
+    text: string;
+};
+
 export type SiteConfig = {
     logo?: Image;
     title: string;
     subtitle?: string;
     description: string;
+    image?: Image;
     headerNavLinks?: Link[];
+    hero?: Hero;
     socialLinks?: Link[];
     postsPerPage?: number;
     projectsPerPage?: number;
@@ -38,6 +45,14 @@ const siteConfig: SiteConfig = {
             href: '/tags'
         }
     ],
+    hero: {
+        title: 'Hi! I am Amandeep',
+        text: `I have been a software developer for almost 8 years,
+        starting out as an android app developer, then branching out to both frontend and backend.
+        This blog is where I explore the inner workings of software, hardware, and whatever else catches my curiosity.
+        If you like peeling back the layers of how things work, stick around — there’s plenty to dig into.
+        \nTo get in touch, my social media handles are at the bottom of the page.`
+    },
     socialLinks: [
         {
             text: 'Github',
@@ -46,6 +61,18 @@ const siteConfig: SiteConfig = {
         {
             text: 'X/Twitter',
             href: 'https://x.com/theamndeepsingh'
+        },
+        {
+            text: 'LinkedIn',
+            href: 'https://linkedin.com/in/amandeepsp'
+        },
+        {
+            text: 'Email',
+            href: 'mailto:amandeepsp@gmail.com'
+        },
+        {
+            text: 'RSS',
+            href: '/rss.xml'
         }
     ],
     postsPerPage: 10,
